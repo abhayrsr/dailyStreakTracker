52efc0b28f124ea48f79a0809d58b2b216afb872
abhayrsr <abhayrajsinghrana123@gmail.com>
2024-04-09 17:07:10 +0530

    tasks list

--- frontend/daily-streak-tracker-fe/src/components/Tasks/tasks.js
+++ frontend/daily-streak-tracker-fe/src/components/Tasks/tasks.js
@@ -1,20 +1,97 @@
+import { useState } from 'react'
 import './tasks.css'
+let id  = 0;
 
 export default function Tasks(){
+    const [text, setText] = useState('');
+    const [tasks, setTasks] = useState([]);
+    
+
+    function handleChange(e){
+        setText(e.target.value)
+    }
+
+    function addTasks(){
+        console.log(id)
+        setTasks([...tasks, {
+            id: id,
+            task: text,
+        }])
+        id++;
+        setText('');
+    }
+
+    function handleDelete(id){
+    
+        setTasks(prevTasks => {
+            const updatedTasks = prevTasks.filter((task) => task.id !== id)
+
+            return updatedTasks.map((task, index) => ({
+                ...task,
+                id: index
+            }))
+        })
+    }
+
+    function handleEdit(id){
+        setTasks()
+    }
+
+    function handleKey(e){
+        if(e.key === "Enter"){
+            addTasks();
+        }
+    }
+
     return(
         <div className="tasks">
-            <div>
+            <div className="enter-tasks">
                 <input
                     className='input-tasks' 
                     type="text"
                     name="text"
-                    value="text"
+                    value={text}
                     placeholder="enter the tasks"
+                    onChange={handleChange}
+                    onKeyDown={handleKey}
                     />
-                <button>
+                <button 
+                    className="add-button"
+                    onClick={addTasks}
+                    >
                     +
                 </button>
             </div>
+
+           {
+            tasks.length !== 0 ? 
+                <ul className="tasks"> 
+                    {tasks.map((task) => (
+                        <div className='entered-tasks'>
+                            <input 
+                                className='checkbox-tasks'
+                                type='checkbox' />
+                            <li key={task.id} className='added-tasks'>
+                            {task.task}
+                            </li>
+                            <button 
+                                key={task.id} 
+                                className='edit-button'
+                                onClick={() => handleEdit(task.id)}>
+                                /
+                            </button>
+                            <button 
+                                key={task.id} 
+                                className='delete-button'
+                                onClick={() => handleDelete(task.id)} >
+                                -
+                            </button>
+                        </div>
+                    ))}
+                </ul>
+        
+                : <div> </div>
+           }
         </div>
     )
 }
