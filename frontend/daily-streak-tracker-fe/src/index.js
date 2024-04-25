3dbffbb231aa4e9f2d6e595beb929c9c75c98b69
abhay <abhayrajsinghrana123@gmail.com>
2024-04-25 16:51:00 +0530

    google auth

--- frontend/daily-streak-tracker-fe/src/index.js
+++ frontend/daily-streak-tracker-fe/src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId="371681908074-duithrbae4dd6qa2c4hu3st6fm6cbgvs.apps.googleusercontent.com">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
-
-
